005f9273fb19ba72b87c0b642061320738b27456
arunk-web <arunk02615@gmail.com>
2026-07-06 21:00:44 +0530

    checking of JWT token

--- backend/src/middleware/authmiddleware.js
+++ backend/src/middleware/authmiddleware.js
@@ -8,7 +8,7 @@ const protect = async (req,res,next) => {
         if(req.headers.authorization &&
             req.headers.authorization.startsWith('Bearer')) {
 
-                token = req.headers.authorization.split('')[1];
+                token = req.headers.authorization.split(' ')[1];
 
                 const decoded = jwt.verify(token,process.env.JWT_SECRET);
 
