3fa25c2d9f9d20214c79adb76129ed5a8873a0d7
arunk-web <arunk02615@gmail.com>
2026-07-09 18:18:02 +0530

    chat route

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {chat, getChats, getChatById, deleteChat,deleteReview} = require('../controllers/chatController');
+const {chat, getChats, getChatById, deleteChat} = require('../controllers/chatController');
 const {protect} = require('../middleware/authMiddleware');
 
 router.post('/',protect,chat);
 router.get('/', protect,getChats);
 router.get('/:id',protect , getChatById);
 router.delete('/:id', protect, deleteChat);
-router.delete('/:id', protect, deleteReview);
+
 
 module.exports = router;
