0e18c74179b240ffc53c313a42943f81a2624f5a
arunk-web <arunk02615@gmail.com>
2026-07-14 11:25:21 +0530

    send msg to AI and get reply also applied rate limiting in this

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -1,9 +1,36 @@
 const Groq = require('groq-sdk');
 const Chat = require('../models/Chat');
+const User = require('../models/User');
 
 const chat = async (req, res) => {
     try{
         const {messages, chatId} = req.body;
+//         1. req.user._id se User fetch karo MongoDB se
+// 2. Check karo — kya 4 ghante ho gaye lastResetTime se?
+//    Haan → messageCount = 0 reset karo, lastResetTime update karo
+// 3. Check karo — messageCount >= 50?
+//    Haan → error do "Limit reached"
+//    Nahi → aage jao
+// 4. AI reply aane ke baad — messageCount++ karo, save karo
+
+        const user = await User.findById(req.user._id);
+        const FOUR_HOURS = 4 * 60 * 60 * 1000;   //IN MS
+        const now = Date.now();
+
+        if(now - new Date(user.lastResetTime).getTime() > FOUR_HOURS) {
+            user.messageCount = 0;
+            user.lastResetTime = now;
+            await user.save();
+        }
+
+        //if 50 msg limit reached
+        if(user.messageCount >= 50) {
+            const timeLeft = FOUR_HOURS - (now - new Date(user.lastResetTime).getTime());
+            const hoursLeft = Math.ceil(timeLeft / (1000 * 60 * 60));
+            return res.status(429).json({
+                message : `Message limit reached. Try again in ${hoursLeft} hours.`
+            })
+        }
 
         const groq = new Groq({apiKey: process.env.GROQ_API_KEY});   //groq initialisation
 
@@ -34,13 +61,17 @@ Be helpful, clear and concise.`
 
         const reply = completion.choices[0].message.content;
 
+        //after getting reply from AI just increase the msgcount 
+        user.messageCount += 1;
+        await user.save();
+
         const title = messages[0]?.content?.slice(0,40) || 'New Chat';
 
         let savedChat;
 
         if(chatId) {
             savedChat = await Chat.findByIdAndUpdate(
-            chatId,
+            chatId,    //wo id usi user ki honi chahiye wrna dusre user ki chat update ho jayegi
             {messages : [...messages, {role: 'assistant', content : reply}]},
             {new: true}
         );
